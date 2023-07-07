1b01ab2c855b146c0d0e549bd21b27f8538bcfc9
Neto test <anna.shuliko@bk.ru>
2023-07-07 16:08:23 +0300

    Add MoviesManager

---
+++ src/test/java/MoviesManagerTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.javaqa.MoviesManager;
+
+public class MoviesManagerTest {
+
+    @Test
+    public void FindAll() {
+        MoviesManager manager = new MoviesManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FilmI() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film I");
+
+        String[] expected = {"Film I"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FilmIII() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FindLast() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
